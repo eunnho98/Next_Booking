e31e2552761448f2f5a9031b915e1b21ed78e941
eunnho <taemin4u@gmail.com>
2023-03-05 05:59:29 +0900

    feat: prisma User 스키마 제작

---
+++ prisma/migrations/20230225092707_/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "email" TEXT,
+    "password" TEXT,
+    "birthday" TEXT,
+    "userImage" TEXT,
+    "accessToken" TEXT,
+    "refreshToken" TEXT
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
