fa9ad126e88ff129184513b617aadbd998316970
eunnho <taemin4u@gmail.com>
2023-03-05 06:03:49 +0900

    feat: bedroom page

---
+++ pages/room/register/building.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegisterRoomBuilding from '@/components/register/RegisterRoomBuilding';
+
+function building() {
+  return <RegisterRoomBuilding />;
+}
+
+export default building;
