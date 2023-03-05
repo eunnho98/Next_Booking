d5e9804b0c5ccc7826e25e41d2d1f5a179368d28
taemin4u <taemin4u@gmail.com>
2023-03-06 02:38:49 +0900

    chore: Initail Setting

--- pages/room/register/bathroom.tsx
+++ pages/room/register/bathroom.tsx
@@ -1,6 +1,10 @@
+import { roomState } from '@/atom/registerRoom';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
 
 function bathroom() {
+  const room = useRecoilValue(roomState);
+  console.log('bath', room);
   return <div>bathroom</div>;
 }
 
