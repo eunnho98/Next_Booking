63ab77c41fb56546056d7b84f9aaf53a50eff87f
taemin4u <taemin4u@khu.ac.kr>
2023-03-06 23:47:03 +0900

    feat: GET: /api/map/location, 현재 위치 가져오는 api

--- pages/api/maps/location.ts
+++ pages/api/maps/location.ts
@@ -12,8 +12,22 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       // latlng: 위도와 경도의 쌍으로 구성된 값, 좌표 <=> 주소 변환에 사용
       const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&language=ko&key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}`;
       const { data } = await axios.get(url);
-      console.log(data);
-      console.log(data.results[0].address_components);
+      const { lat, lng } = data.results[0].geometry.location;
+      const addressComponents = data.results[0].address_components;
+
+      // ! 현재 위치가 서울인 경우 district(시)가 없음 -> 경우 나누기 필요
+      const results = {
+        latitude: lat,
+        longitude: lng,
+        country: addressComponents[5].long_name,
+        city: addressComponents[4].long_name,
+        district: addressComponents[3].long_name,
+        streetAddress: `${addressComponents[1].long_name} ${addressComponents[0].long_name}`,
+        postcode: addressComponents[6].long_name,
+        political: addressComponents[2].long_name,
+      };
+      res.statusCode = 200;
+      res.send(results);
     } catch (error) {
       res.statusCode = 404;
       return res.end();
