937142266643851cbfcce8aef0e62d0b557f56ec
taemin4u <taemin4u@khu.ac.kr>
2023-03-07 14:59:19 +0900

    add: 편의 시설 페이지

---
+++ pages/room/register/amentites.tsx
@@ -0,0 +1,8 @@
+import RegisterRoomAmentities from '@/components/register/RegisterRoomAmentities';
+import React from 'react';
+
+function amentities() {
+  return <RegisterRoomAmentities />;
+}
+
+export default amentities;
