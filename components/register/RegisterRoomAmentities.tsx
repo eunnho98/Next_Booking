40e8d5335be5a623bc05af8d2b72011b0c3ede10
taemin4u <taemin4u@khu.ac.kr>
2023-03-09 13:26:28 +0900

    feat: 편의시설 페이지 컴포넌트

--- components/register/RegisterRoomAmentities.tsx
+++ components/register/RegisterRoomAmentities.tsx
@@ -5,32 +5,64 @@ import React from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 import { useRecoilState } from 'recoil';
 import CommonCheckBox from '../common/CommonCheckBox';
+import RegisterFooter from './RegisterFooter';
 
 interface IForm extends FieldValues {
   conveniences: string;
 }
 function RegisterRoomAmentities() {
   const [room, setRoom] = useRecoilState(roomState);
-  const { handleSubmit, control, watch } = useForm<IForm>();
+  const { handleSubmit, control, watch, getValues } = useForm<IForm>();
   console.log(watch());
-  return (
-    <Box p="62px 30px 100px">
-      <Text as="h2" fontSize="24px" fontWeight="800" mb="50px">
-        편의 시설을 선택해주세요.
-      </Text>
-      <Text as="h3" fontWeight="bold" mb="6px" color="gray.700">
-        Step 5.
-      </Text>
-      <Text as="p" fontSize="14px" maxW="400px" mb="24px" wordBreak="keep-all">
-        묵을 숙소의 편의 시설들을 선택해주세요.
-      </Text>
+  const onSubmit = () => {
+    const amentities = getValues('conveniences') as unknown as string[];
+    const newArr = amentities.filter((ele) => {
+      return ele != null;
+    });
 
-      <CommonCheckBox
-        control={control}
-        name="conveniences"
-        options={convinienceList}
+    setRoom((prev) => {
+      return {
+        ...prev,
+        amentities: newArr,
+      };
+    });
+  };
+  return (
+    <form
+      onSubmit={() => {
+        handleSubmit(onSubmit)();
+      }}
+    >
+      <Box p="62px 30px 100px">
+        <Text as="h2" fontSize="24px" fontWeight="800" mb="50px">
+          편의 시설을 선택해주세요.
+        </Text>
+        <Text as="h3" fontWeight="bold" mb="6px" color="gray.700">
+          Step 5.
+        </Text>
+        <Text
+          as="p"
+          fontSize="14px"
+          maxW="400px"
+          mb="42px"
+          wordBreak="keep-all"
+        >
+          묵을 숙소의 편의 시설들을 선택해주세요.
+        </Text>
+
+        <CommonCheckBox
+          control={control}
+          name="conveniences"
+          options={convinienceList}
+        />
+      </Box>
+      <RegisterFooter
+        prevLink="/room/reigster/geometry"
+        nextLink="/room/register/photo"
+        onSubmit={onSubmit}
+        isValid={true}
       />
-    </Box>
+    </form>
   );
 }
 
