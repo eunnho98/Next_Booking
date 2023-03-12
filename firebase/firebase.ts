a64c885adada8e5c8d85c0efabbc94bd1ca27a0c
taemin4u <taemin4u@khu.ac.kr>
2023-03-13 02:41:23 +0900

    feat: firebase storage 설정

---
+++ firebase/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECDT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+//* firebaseConfig 정보로 firebase 시작
+const fbaseApp = initializeApp(firebaseConfig);
+
+//* firebase의 storage 인스턴스를 변수에 저장
+export const fstorage = getStorage(fbaseApp);
