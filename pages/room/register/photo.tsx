e69c27b8c4d328ef9f58337710c24e95d43abf9a
taemin4u <taemin4u@khu.ac.kr>
2023-03-13 04:24:41 +0900

    feat: getStaticProps로 fireStore에서 사진 전부 가져오기

--- pages/room/register/photo.tsx
+++ pages/room/register/photo.tsx
@@ -1,8 +1,31 @@
 import RegisterRoomPhoto from '@/components/register/RegisterRoomPhoto';
+import { fstorage } from '@/firebase/firebase';
+import { ref, listAll, getDownloadURL } from 'firebase/storage';
+import { GetStaticPropsResult, InferGetStaticPropsType } from 'next/types';
 import React from 'react';
 
-function photo() {
-  return <RegisterRoomPhoto />;
+type Props = {
+  images: string[];
+};
+export async function getStaticProps(): Promise<{ props: Props }> {
+  const fileRef = ref(fstorage, 'image/');
+  const result = await listAll(fileRef);
+  const urls = await Promise.all(
+    result.items.map(async (item) => {
+      const url = await getDownloadURL(item);
+      return url;
+    }),
+  );
+
+  return {
+    props: {
+      images: urls,
+    },
+  };
+}
+
+function photo({ images }: Props) {
+  return <RegisterRoomPhoto images={images} />;
 }
 
 export default photo;
