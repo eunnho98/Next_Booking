94fb4ed50060db126a832afd16cd69fa9cb855c0
taemin4u <taemin4u@khu.ac.kr>
2023-03-13 17:49:45 +0900

    add: description page

---
+++ pages/room/register/description.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import RegisterRoomDescription from '@/components/register/RegisterRoomDescription';
+function description() {
+  return <RegisterRoomDescription />;
+}
+
+export default description;
