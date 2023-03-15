315017457de9ea61ab13caf81919aef3edb3b29b
taemin4u <taemin4u@khu.ac.kr>
2023-03-16 02:39:35 +0900

    feat: 이니시스, 카카오페이 ,토스페이 결제

---
+++ components/PurchaseModal.tsx
@@ -0,0 +1,217 @@
+import { dateState, initialState } from '@/atom/atom';
+import { guestNum, roomState } from '@/atom/registerRoom';
+import { SaveDataAPI } from '@/lib/api/file';
+import { RequestPayParams, RequestPayResponse } from '@/lib/type';
+import {
+  Box,
+  Button,
+  HStack,
+  ListItem,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  UnorderedList,
+} from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+
+export type RequestPayResponseCallback = (response: RequestPayResponse) => void;
+
+export interface Iamport {
+  init: (accountID: string) => void;
+  request_pay: (
+    params: RequestPayParams,
+    callback?: RequestPayResponseCallback,
+  ) => void;
+}
+
+declare global {
+  interface Window {
+    IMP?: Iamport;
+  }
+}
+
+interface IProps {
+  isOpen: boolean;
+  onClose: () => void;
+  diffDate: number;
+}
+
+function PurchaseModal({ isOpen, onClose, diffDate }: IProps) {
+  const { IMP } = window;
+  const [room, setRoom] = useRecoilState(roomState);
+  const guest = useRecoilValue(guestNum);
+  const user = useRecoilValue(initialState);
+  const date = useRecoilValue(dateState);
+  const price =
+    guest.adults * 50000 +
+    guest.children * 10000 +
+    guest.teenager * 30000 +
+    diffDate * 50000;
+  const priceString = price.toLocaleString('ko-KR');
+
+  useEffect(() => {
+    const jquery = document.createElement('script');
+    jquery.src = 'https://code.jquery.com/jquery-1.12.4.min.js';
+
+    const iamport = document.createElement('script');
+    iamport.src = 'https://cdn.iamport.kr/v1/iamport.js';
+
+    document.head.appendChild(jquery);
+    document.head.appendChild(iamport);
+
+    return () => {
+      document.head.removeChild(jquery);
+      document.head.removeChild(iamport);
+    };
+  }, []);
+  const handlePayment = (pgType: 'tosspay' | 'kakaopay' | 'html5_inicis') => {
+    IMP?.init('imp70004432');
+    IMP?.request_pay(
+      {
+        // kakaopay
+        // html5_inicis
+
+        pg: pgType,
+        pay_method: 'card',
+        merchant_uid: 'merchant_' + new Date().getTime(),
+        name: '결제테스트',
+        amount: price,
+        buyer_email: user.email,
+        buyer_name: user.name,
+        buyer_tel: '010-1234-5678',
+        buyer_addr: '서울특별시 강남구 삼성동',
+        buyer_postcode: '123-456',
+      },
+      (res: RequestPayResponse) => {
+        const { success, error_msg } = res;
+        if (success) {
+          alert('결제 성공');
+        } else {
+          alert(`결제 실패, ${error_msg}`);
+        }
+      },
+    );
+  };
+
+  const onClick = async () => {
+    setRoom((prev) => {
+      return {
+        ...prev,
+        price: price,
+      };
+    });
+    try {
+      const body = {
+        userId: user.id,
+        largeBuildingType: room.largeBuildingType!,
+        buildingType: room.buildingType!,
+        roomType: room.roomType!,
+        bedroomCount: room.bedroomCount,
+        bedCount: room.bedCount,
+        bedList: room.bedList,
+        bathroomCount: room.bathroomCount,
+        country: room.country,
+        city: room.city,
+        district: room.district,
+        political: room.political,
+        streetAddress: room.streetAddress,
+        postcode: room.postcode,
+        latitude: room.latitude,
+        longitude: room.longitude,
+        amentities: room.amentities,
+        roomImage: room.roomImage,
+        description: room.description,
+        price: room.price,
+        checkIn: date.checkIn,
+        checkOut: date.checkOut,
+        adults: guest.adults,
+        teenager: guest.teenager,
+        children: guest.children,
+      };
+
+      const res = await SaveDataAPI(body);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <form>
+          <ModalContent>
+            <ModalHeader>결제 페이지</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <HStack justifyContent="space-between">
+                <Box>
+                  <UnorderedList>
+                    <ListItem>성인 {guest.adults}명</ListItem>
+                    <ListItem>청소년 {guest.teenager}명</ListItem>
+                    <ListItem>
+                      어린이
+                      {guest.children}명
+                    </ListItem>
+                  </UnorderedList>
+                  <Text mt="8px" fontSize="lg" fontWeight="bold">
+                    숙박기간: {diffDate}일
+                  </Text>
+                </Box>
+                <Box>
+                  <Text fontSize="48px" color="pink.600" fontWeight="bold">
+                    {priceString}￦
+                  </Text>
+                </Box>
+              </HStack>
+            </ModalBody>
+            <ModalFooter justifyContent="space-evenly">
+              <Button
+                colorScheme="red"
+                onClick={() => {
+                  handlePayment('html5_inicis');
+                }}
+              >
+                결제하기
+              </Button>
+              <Button
+                backgroundColor="#fae100"
+                _hover={{
+                  bgColor: 'yellow.400',
+                }}
+                onClick={() => {
+                  handlePayment('kakaopay');
+                }}
+              >
+                카카오페이
+              </Button>
+              <Button
+                bgColor="#0c61eb"
+                color="white"
+                _hover={{
+                  bgColor: '#063a8f',
+                }}
+                onClick={() => {
+                  handlePayment('tosspay');
+                }}
+              >
+                토스페이
+              </Button>
+            </ModalFooter>
+            <ModalFooter>
+              <Button onClick={onClick}>담기</Button>
+            </ModalFooter>
+          </ModalContent>
+        </form>
+      </Modal>
+    </>
+  );
+}
+
+export default PurchaseModal;
--- components/register/RoomRegisterPrice.tsx
+++ components/register/RoomRegisterPrice.tsx
@@ -1,6 +1,22 @@
+import { dateState } from '@/atom/atom';
+import { guestNum, roomState } from '@/atom/registerRoom';
 import { RequestPayParams, RequestPayResponse } from '@/lib/type';
-import { Button } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  Text,
+  Image,
+  Heading,
+  Stack,
+  useDisclosure,
+} from '@chakra-ui/react';
 import React, { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import PurchaseModal from '../PurchaseModal';
+import UpdateModal from '../UpdateModal';
+import RegisterFooter from './RegisterFooter';
 
 export type RequestPayResponseCallback = (response: RequestPayResponse) => void;
 
@@ -18,62 +34,122 @@ declare global {
   }
 }
 function RoomRegisterPrice() {
-  const { IMP } = window;
-
-  useEffect(() => {
-    const jquery = document.createElement('script');
-    jquery.src = 'https://code.jquery.com/jquery-1.12.4.min.js';
-
-    const iamport = document.createElement('script');
-    iamport.src = 'https://cdn.iamport.kr/v1/iamport.js';
-
-    document.head.appendChild(jquery);
-    document.head.appendChild(iamport);
-
-    return () => {
-      document.head.removeChild(jquery);
-      document.head.removeChild(iamport);
-    };
-  }, []);
-
-  const handlePayment = () => {
-    console.log('아');
-    IMP?.init('imp70004432');
-    IMP?.request_pay(
-      {
-        // kakaopay
-        // html5_inicis
-
-        pg: 'tosspay',
-        pay_method: 'card',
-        merchant_uid: 'merchant_' + new Date().getTime(),
-        name: '결제테스트',
-        amount: 100,
-        buyer_email: 'iamport@siot.do',
-        buyer_name: '구매자',
-        buyer_tel: '010-1234-5678',
-        buyer_addr: '서울특별시 강남구 삼성동',
-        buyer_postcode: '123-456',
-      },
-      (res: RequestPayResponse) => {
-        const { success, error_msg } = res;
-        if (success) {
-          alert('결제 성공');
-        } else {
-          alert(`결제 실패, ${error_msg}`);
-        }
-      },
-    );
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const onSubmit = () => {
+    onOpen();
   };
+  const room = useRecoilValue(roomState);
+  const date = useRecoilValue(dateState);
+  const guest = useRecoilValue(guestNum);
+  const diffMSec =
+    new Date(date.checkOut).getTime() - new Date(date.checkIn).getTime();
+  const diffDate = diffMSec / (24 * 60 * 60 * 1000);
+  const location =
+    room.country +
+    ' ' +
+    room.city +
+    ' ' +
+    room.district +
+    ' ' +
+    room.political +
+    ' ' +
+    room.streetAddress +
+    ' ' +
+    room.postcode;
+
   return (
-    <Button
-      onClick={() => {
-        handlePayment();
-        console.log('sdks?');
-      }}
-    >
-      결제
-    </Button>
+    <form>
+      <Box p="62px 30px 100px">
+        <Text as="h2" fontSize="24px" fontWeight="800" mb="50px">
+          결제하기
+        </Text>
+        <Text as="h3" fontWeight="bold" mb="6px" color="gray.700">
+          Step 7.
+        </Text>
+        <Text
+          as="p"
+          fontSize="14px"
+          maxW="400px"
+          mb="24px"
+          wordBreak="keep-all"
+        >
+          선택하신 숙소의 정보입니다.
+          <br />
+          성인 인당 5만원, 청소년 인당 3만원, 어린이 인당 1만원입니다.
+          <br />
+          1박당 5만원이 부과됩니다.
+        </Text>
+        <Card maxW="md">
+          <CardBody position="relative">
+            <Image w="100%" h="300px" src={room.roomImage} borderRadius="lg" />
+
+            <Heading size="sm" mt="16px">
+              방 유형
+            </Heading>
+            <Text>
+              {room.largeBuildingType}, {room.buildingType}
+            </Text>
+            <Heading size="sm" mt="8px">
+              위치
+            </Heading>
+            <Text>{location}</Text>
+            <Heading size="sm" mt="8px">
+              방 개수
+            </Heading>
+            <Text>{room.bedroomCount}개</Text>
+            <Heading size="sm" mt="8px">
+              침대 개수 및 유형
+            </Heading>
+            {room.bedList.map((bedroom) => (
+              <>
+                <Text as="span" key={bedroom.id}>
+                  {bedroom.id}번방:{' '}
+                </Text>
+                {bedroom.beds.map((detail, index) => (
+                  <Text as="span" key={index}>
+                    {detail.type} {detail.count}개{' '}
+                  </Text>
+                ))}
+                <br />
+              </>
+            ))}
+            <Heading size="sm" mt="8px">
+              편의시설
+            </Heading>
+            {room.amentities.map((amen) => (
+              <Text as="span">{amen} </Text>
+            ))}
+            <Heading size="sm" mt="8px">
+              숙박 기간
+            </Heading>
+            <Text>
+              {date.checkIn} ~ {date.checkOut}
+            </Text>
+            <Heading size="sm" mt="8px">
+              숙박 인원
+            </Heading>
+            <Text>
+              성인 {guest.adults}명, 청소년 {guest.teenager}명, 어린이{' '}
+              {guest.children}명
+            </Text>
+          </CardBody>
+        </Card>
+      </Box>
+      {/* <Button
+        onClick={() => {
+          handlePayment();
+          console.log('sdks?');
+        }}
+      >
+        결제
+      </Button> */}
+      <RegisterFooter
+        prevLink="/room/register/description"
+        onSubmit={onSubmit}
+        isValid={true}
+      />
+      <PurchaseModal isOpen={isOpen} onClose={onClose} diffDate={diffDate} />
+    </form>
   );
 }
 
