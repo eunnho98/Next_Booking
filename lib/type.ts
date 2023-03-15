001d213ab8133d6cd1b40fee304ecf0a5e24ba7a
taemin4u <taemin4u@khu.ac.kr>
2023-03-16 02:39:09 +0900

    add: 결제관련 type 추가

--- lib/type.ts
+++ lib/type.ts
@@ -8,6 +8,7 @@ export type StoredUserType = {
   userImage?: string;
   accessToken?: string;
   refreshToken?: string;
+  isPurchase?: boolean;
 };
 
 // 클라이언트 반환용, 패스워드 삭제
@@ -22,6 +23,7 @@ export type UserType = {
 
 export type UserState = UserType & {
   isLogged: boolean;
+  isPurchase: boolean;
 };
 
 // 침대 유형
@@ -39,3 +41,67 @@ export type BedType =
   | '유아용 침대'
   | '해먹'
   | '물침대';
+
+// payment type
+export interface RequestPayAdditionalParams {
+  digital?: boolean;
+  vbank_due?: string;
+  m_redirect_url?: string;
+  app_scheme?: string;
+  biz_num?: string;
+}
+
+export interface Display {
+  card_quota?: number[];
+}
+
+export interface RequestPayParams extends RequestPayAdditionalParams {
+  pg?: string;
+  pay_method: string;
+  escrow?: boolean;
+  merchant_uid: string;
+  name?: string;
+  amount: number;
+  custom_data?: any;
+  tax_free?: number;
+  currency?: string;
+  language?: string;
+  buyer_name?: string;
+  buyer_tel: string;
+  buyer_email?: string;
+  buyer_addr?: string;
+  buyer_postcode?: string;
+  notice_url?: string | string[];
+  display?: Display;
+}
+
+// payment callback func type
+export interface RequestPayAdditionalResponse {
+  apply_num?: string;
+  vbank_num?: string;
+  vbank_name?: string;
+  vbank_holder?: string | null;
+  vbank_date?: number;
+}
+
+export interface RequestPayResponse extends RequestPayAdditionalResponse {
+  success: boolean;
+  error_code: string;
+  error_msg: string;
+  imp_uid: string | null;
+  merchant_uid: string;
+  pay_method?: string;
+  paid_amount?: number;
+  status?: string;
+  name?: string;
+  pg_provider?: string;
+  pg_tid?: string;
+  buyer_name?: string;
+  buyer_email?: string;
+  buyer_tel?: string;
+  buyer_addr?: string;
+  buyer_postcode?: string;
+  custom_data?: any;
+  paid_at?: number;
+  receipt_url?: string;
+}
